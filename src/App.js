8e2487ded217ae2a09a39a9117ccda0127484e51
unknown <mexrojiddinazimov98@gmail.com>
2021-08-25 00:03:18 -0700

    mexrojiddin add page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Button from '@material-ui/core/Button';
+import ReactPlayer from "react-player"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <nav>
+    <div className="foo">
+        <div className="nav-bar">
+          HOME
+        </div>
+        <div className="nav-bar">
+          PORTFOLIO
+        </div>
+        <div className="nav-bar">
+           ABOUT US
+        </div>
+        <div className="nav-bar">
+            CONTACT
+        </div>
     </div>
+    </nav>
+    <div className="video">
+      <ReactPlayer url="https://youtu.be/JtVnOZ26XHA"/>
+    </div>
+    <div className="text">
+      <h1>OUR STRONG</h1>
+      <h1>ORGANAISATION</h1>
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing <br/> elit. Sequi asperiores ullam libero a quaerat voluptatum maxime <br/> quos dolorem incidunt quasi!</h3>
+      <h4><Button variant="contained" color="secondary">
+      Contact Us
+      </Button></h4>
+    </div>
+  </div>
   );
 }
 
