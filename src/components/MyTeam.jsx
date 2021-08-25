0015f5386be74dc2d276c478692e652cd2fa8205
Dostonbek2000 <simple2000code@gmail.com>
2021-08-25 18:51:02 +0500

    Dostonbek

---
+++ src/components/MyTeam.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const MyTeam = () => {
+    return (
+        <div className='teampart'>
+            <div className="teamtext">MEET OUR TEAM</div>
+            <p className="lorem">
+                Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Illum,
+                veniam ipsam placeat <br /> ipsum quo doloribus quos quia numquam excepturi
+            </p>
+            <p className="bordertag"></p>
+            <div className="nextdiv">
+                <div className="one">
+                    4X6
+                </div>
+                <div className="second">
+                    <p className="text1">MAke Weighb</p>
+                    <p className="loremm">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Quia maxime doloribus omnis repellat? Saepe sequi
+                    </p>
+                    <div className="links">
+                        <a href="">Facebook</a>
+                        <a href="">Dribble</a>
+                        <a href="">Behance</a>
+                        <a href="">Twitter</a>
+
+                    </div>
+                    <div className='poabs'>
+                        <div className="squares">
+                            <div className="sq101">
+                                2x2
+                            </div>
+                            <div className="sq102">
+                               2x2
+                            </div>
+                            <div className="sq103">
+                               2x2
+                            </div>
+                            <div className="sq104">
+                               2x2
+                            </div>
+                        </div>
+                        <div className="hoversq">
+                        <div className="sq12">
+                                John Smith
+                            </div>
+                            <div className="sq123">
+                                William
+                            </div>
+                            <div className="sq124">
+                                Selena
+                            </div>
+                            <div className="sq125">
+                                Arnold
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MyTeam
